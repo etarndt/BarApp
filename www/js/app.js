f19fb6c484ad3efaf1619747351b488e88ce2585
Eric Arndt <etarndt@wisc.edu>
2017-03-31 12:50:06 -0500

    Made some minor changes to try and get Firebase Database working. Didn’t get it to work.

--- www/js/app.js
+++ www/js/app.js
@@ -41,19 +41,38 @@ angular.module('starter', ['ionic', 'ngCordova'])
 
         $cordovaGeolocation.getCurrentPosition(options).then(function(position){
 
-            var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            var latLng = new google.maps.LatLng(43.072932, -89.396565);
 
             var mapOptions = {
                 center: latLng,
-                zoom: 15,
+                zoom: 17,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
 
             $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            //Wait until the map is loaded
+            google.maps.event.addListenerOnce($scope.map, 'idle', function(){
 
+                var marker = new google.maps.Marker({
+                    map: $scope.map,
+                    animation: google.maps.Animation.DROP,
+                    position: latLng
+                });
+
+                var infoWindow = new google.maps.InfoWindow({
+                    content: "Here I am!"
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    infoWindow.open($scope.map, marker);
+                });
+
+            });
         }, function(error){
             console.log("Could not get location");
         });
+
+
     });
 
 // test
