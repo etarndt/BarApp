816d123422805afb63ddde7f45cf8c3c89e3362e
Brett Geschke <bgeschke@wisc.edu>
2017-04-16 14:41:53 -0500

    Forgot to commit the files.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngCordova'])
+var barApp = angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,17 +26,73 @@ angular.module('starter', ['ionic', 'ngCordova'])
     .config(function($stateProvider, $urlRouterProvider) {
 
         $stateProvider
-            .state('map', {
-                url: '/map',
-                templateUrl: 'map.html',
-                controller: 'MapCtrl'
-            });
+            .state('tabs', {
+                url: '/tab',
+                    templateUrl: 'templates/tabs.html',
+            })
+            .state('tabs.overview', {
+                url: '/overview',
+                views: {
+                    'overview-tab': {
+                        templateUrl: 'templates/overview.html',
+                        controller: 'overviewCtrl'
+                    }
+                }
+            })
+            .state('tabs.add', {
+                url:'/add',
+                views: {
+                    'add-tab': {
+                        templateUrl: 'templates/add.html',
+                        controller: 'buttonCtrl'
+                    }
+                }
+            })
+            .state('tabs.summary', {
+                url:'/summary',
+                views: {
+                    'summary-tab': {
+                        templateUrl: 'templates/summary.html'
+                    }
+                }
+            })
+            .state('settings', {
+                url: '/settings',
+                templateUrl: 'templates/settings.html',
+                controller: 'buttonCtrl'
+            })
+            .state('help', {
+                url: '/help',
+                templateUrl: 'templates/help.html',
+                controller: 'buttonCtrl'
+            })
+        .state('line', {
+            url: '/line',
+            templateUrl: 'templates/line.html',
+            controller: 'buttonCtrl'
+        });
 
-        $urlRouterProvider.otherwise("/map");
 
-    })
+        $urlRouterProvider.otherwise("/tab/overview");
 
-    .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+    });
+
+    // array to store bar information - not working
+    // var bars = [
+    //     {
+    //         bar: 'The Double U',
+    //         lat: '43.0734',
+    //         lon: '-89.3968'
+    //     },
+    //     {
+    //         bar: 'Kollege Klub',
+    //         lat: '43.0756',
+    //         lon: '-89.3971'
+    //     }
+    // ]
+
+
+    barApp.controller('overviewCtrl', function($scope, $state, $cordovaGeolocation) {
         var options = {timeout: 10000, enableHighAccuracy: true};
 
         $cordovaGeolocation.getCurrentPosition(options).then(function(position){
@@ -50,29 +106,73 @@ angular.module('starter', ['ionic', 'ngCordova'])
             };
 
             $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+
             //Wait until the map is loaded
             google.maps.event.addListenerOnce($scope.map, 'idle', function(){
 
-                var marker = new google.maps.Marker({
-                    map: $scope.map,
-                    animation: google.maps.Animation.DROP,
-                    position: latLng
-                });
+                //Additional Markers //
+                // $scope.markers= [];
 
                 var infoWindow = new google.maps.InfoWindow({
                     content: "Here I am!"
                 });
+                 // var createMarker = function(info) { // function could be called to drop multiple pins
 
-                google.maps.event.addListener(marker, 'click', function () {
-                    infoWindow.open($scope.map, marker);
-                });
+                    var marker = new google.maps.Marker({
+                        map: $scope.map,
+                        animation: google.maps.Animation.DROP,
+                        position: latLng,
+                        //position: new google.maps.LatLng(info.lat, info.long),
+                        //title: info.city
+
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function () {
+                        // infoWindow.setContent('<h2>' + marker.title + '</h2>')
+                        infoWindow.open($scope.map, marker);
+                    });
+                    // $scope.markers.push(marker);
+                // }
+                // would loop through this - again, can't get it to work.
+                // for (i = 0; i < bars.length; i++) {
+                //     createMarker(bars[i]);
+                //  }
 
             });
         }, function(error){
             console.log("Could not get location");
         });
 
+        $scope.settingsClick = function() {
+            $state.go('settings');
+        }
+
+
+
 
     });
 
-// test
+barApp.controller('buttonCtrl', function($scope, $state, $cordovaGeolocation) {
+
+    $scope.back = function() {
+        $state.go('tabs.overview');
+    }
+
+    $scope.backCreate = function() {
+        $state.go('tabs.add');
+    }
+
+    $scope.help = function() {
+        $state.go('help');
+    }
+
+    $scope.line = function() {
+        $state.go('line');
+    }
+    $scope.submit = function() {
+        $state.go('tabs.overview');
+    }
+
+
+});
