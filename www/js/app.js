aad0e547c71c5a9979cedc1ba11ddb01b890ae85
Eric Arndt <etarndt@wisc.edu>
2017-04-16 14:58:12 -0500

    Made sure Brett’s commit integrated with this commit. Was able to grab bar data from the database to drop multiple pins on the map. Also, experimented with custom markers with bar logos. Bugs that Brett mentioned still exist.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var barApp = angular.module('starter', ['ionic', 'ngCordova'])
+var barApp = angular.module('starter', ['ionic', 'ngCordova', "firebase"])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -95,54 +95,117 @@ var barApp = angular.module('starter', ['ionic', 'ngCordova'])
     barApp.controller('overviewCtrl', function($scope, $state, $cordovaGeolocation) {
         var options = {timeout: 10000, enableHighAccuracy: true};
 
-        $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+        // keep for testing
 
-            var latLng = new google.maps.LatLng(43.072932, -89.396565);
+        // $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+        //
+        //     var latLng = new google.maps.LatLng(43.072932, -89.396565);
+        //
+        //     var mapOptions = {
+        //         center: latLng,
+        //         zoom: 17,
+        //         mapTypeId: google.maps.MapTypeId.ROADMAP
+        //     };
+        //
+        //     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        //     //Wait until the map is loaded
+        //     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+        //
+        //         var marker = new google.maps.Marker({
+        //             map: $scope.map,
+        //             animation: google.maps.Animation.DROP,
+        //             position: latLng
+        //         });
+        //
+        //         var infoWindow = new google.maps.InfoWindow({
+        //             content: "Here I am!"
+        //         });
+        //
+        //         google.maps.event.addListener(marker, 'click', function () {
+        //             infoWindow.open($scope.map, marker);
+        //         });
+        //
+        //     });
+        // }, function(error){
+        //     console.log("Could not get location");
+        // });
 
+        // Map Settings //
+        $scope.initialise = function() {
+            var myLatlng = new google.maps.LatLng(43.072932, -89.396565);
             var mapOptions = {
-                center: latLng,
-                zoom: 17,
+                center: myLatlng,
+                zoom: 16,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             };
+            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            // Geo Location /
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                map.setCenter(new google.maps.LatLng(43.072932, -89.396565));
+                var myLocation = new google.maps.Marker({
+                    position: new google.maps.LatLng(43.072932, -89.396565),
+                    map: map,
+                    animation: google.maps.Animation.DROP,
+                    title: "My Location",
+                    content: "hardcoded at Grainger"
 
-            $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-
-            //Wait until the map is loaded
-            google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-
-                //Additional Markers //
-                // $scope.markers= [];
-
-                var infoWindow = new google.maps.InfoWindow({
-                    content: "Here I am!"
                 });
-                 // var createMarker = function(info) { // function could be called to drop multiple pins
-
-                    var marker = new google.maps.Marker({
-                        map: $scope.map,
-                        animation: google.maps.Animation.DROP,
-                        position: latLng,
-                        //position: new google.maps.LatLng(info.lat, info.long),
-                        //title: info.city
-
-                    });
-
-                    google.maps.event.addListener(marker, 'click', function () {
-                        // infoWindow.setContent('<h2>' + marker.title + '</h2>')
-                        infoWindow.open($scope.map, marker);
-                    });
-                    // $scope.markers.push(marker);
-                // }
-                // would loop through this - again, can't get it to work.
-                // for (i = 0; i < bars.length; i++) {
-                //     createMarker(bars[i]);
-                //  }
-
+                google.maps.event.addListener(myLocation, 'click', function(){
+                    infoWindow.setContent('<h2>' + myLocation.title + '</h2>' + myLocation.content);
+                    infoWindow.open($scope.map, myLocation);
+                });
             });
-        }, function(error){
-            console.log("Could not get location");
-        });
+            $scope.map = map;
+            // Additional Markers //
+            $scope.markers = [];
+            var infoWindow = new google.maps.InfoWindow();
+            var createMarker = function (info){
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(info.lat, info.long),
+                    map: $scope.map,
+                    animation: google.maps.Animation.DROP,
+                    title: info.bar,
+                    icon: "img/" + info.icon
+                });
+                marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+                google.maps.event.addListener(marker, 'click', function(){
+                    infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+                    infoWindow.open($scope.map, marker);
+                });
+                $scope.markers.push(marker);
+            }
+
+            // keep for testing
+
+            // for (i = 0; i < cities.length; i++){
+            //     createMarker(cities[i]);
+            // }
+
+            $scope.retrieve_data = function() {
+
+
+                return firebase.database().ref('Bars').once('value').then(function(snapshot) {
+                    console.log(snapshot.val()[1])
+
+                    $scope.ListOfBars = []
+                    ListOfBars = snapshot.val()
+                    console.log(ListOfBars[1]['lat'])
+
+                    console.log(snapshot.numChildren());
+                    for (i = 0; i < snapshot.numChildren(); i++){
+                        createMarker(ListOfBars[i+1]);
+                    }
+                    // console.log(cities[1])
+                });
+
+
+            }
+
+            // keep this here but can be cleaned up eventually
+            $scope.retrieve_data()
+
+        };
+        google.maps.event.addDomListener(document.getElementById("map"), 'load', $scope.initialise());
 
         $scope.settingsClick = function() {
             $state.go('settings');
